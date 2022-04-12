e531137baf972cb5f0a798e4bc5eae4a494e58d2
Livia <livia.vianna8@gmail.com>
2022-04-12 18:12:45 -0300

    commit

---
+++ README.md
@@ -0,0 +1,20 @@
+﻿![](https://64.media.tumblr.com/64547d6faa128786b462d8b0932529e0/b2c9356e175fd317-8a/s2048x3072/7816aef9c83cc280142777d69aafc6ca66acc1ce.pnj)
+## Landing Page com FlexBox
+Landing Page e re-design da página Web da marca **Kylie Skin** por Kylie Jenner na versão PT-BR. Feita com Flexbox. Inspirações: [Behance](https://www.behance.net/gallery/138960431/REN-Website-Redesign?tracking_source=search_projects%7Cmac%20cosmetics%20ui) , [Rare Beauty ](https://www.rarebeauty.com/)e próprio site da [Kylie Skin](https://kyliecosmetics.com/en-us/kylie-skin).
+## Mobile
+![Mobile](https://64.media.tumblr.com/4518d16af38a6144fee610eead906d95/01082b6d1e6a4404-25/s540x810/f8609c798240cf58952a3188f445f5d3c0241b81.pnj)
+## Linguagens Utilizadas
+
+ - HTML
+ - CSS
+ - JavaScript.
+## Link 
+
+## Cores Utilizadas
+![enter image description here](https://64.media.tumblr.com/7d185975cbf47c73814c22363f625514/971c4ac2321d2c85-f4/s100x200/68005953e23e6fde08924cb5df2ddfa49bce80fe.pnj)
+![enter image description here](https://64.media.tumblr.com/948bf906862a3c2413e52fe3dcb7be52/971c4ac2321d2c85-4f/s100x200/57d6601633b2ae3bb01b42a623170a4a5c7d3bf6.pnj)
+![enter image description here](https://64.media.tumblr.com/973984225f67952d5add7104bd200b22/971c4ac2321d2c85-d6/s100x200/6dc1d9326b065a5273abd599f9072a19c42f8cfe.pnj)
+![enter image description here](https://64.media.tumblr.com/cd35379026665de7cb966cf8e70206e2/971c4ac2321d2c85-1e/s100x200/a8bdd34b5103e1174c714db0fc9a852db7c225a3.pnj)
+## Fonte
+Inter.
+
---
+++ script.js
@@ -0,0 +1,69 @@
+class MobileNavbar {
+    constructor(mobileMenu, navList, navLinks) {
+      this.mobileMenu = document.querySelector(mobileMenu);
+      this.navList = document.querySelector(navList);
+      this.navLinks = document.querySelectorAll(navLinks);
+      this.activeClass = "active";
+  
+      this.handleClick = this.handleClick.bind(this);
+    }
+  
+    animateLinks() {
+      this.navLinks.forEach((link, index) => {
+        link.style.animation
+          ? (link.style.animation = "")
+          : (link.style.animation = `navLinkFade 0.5s ease forwards ${
+              index / 7 + 0.3
+            }s`);
+      });
+    }
+  
+    handleClick() {
+      this.navList.classList.toggle(this.activeClass);
+      this.mobileMenu.classList.toggle(this.activeClass);
+      this.animateLinks();
+    }
+  
+    addClickEvent() {
+      this.mobileMenu.addEventListener("click", this.handleClick);
+    }
+  
+    init() {
+      if (this.mobileMenu) {
+        this.addClickEvent();
+      }
+      return this;
+    }
+  }
+  
+  const mobileNavbar = new MobileNavbar(
+    ".mobile-menu",
+    ".nav-list",
+    ".nav-list li",
+  );
+  mobileNavbar.init();
+const scrollToTopButton = document.getElementById('js-top');
+
+const scrollFunc = () => {
+  let y = window.scrollY;
+  
+  if (y > 0) {
+    scrollToTopButton.className = "top-link show";
+  } else {
+    scrollToTopButton.className = "top-link hide";
+  }
+};
+
+window.addEventListener("scroll", scrollFunc);
+
+const scrollToTop = () => {
+  const c = document.documentElement.scrollTop || document.body.scrollTop;
+  if (c > 0) {
+    window.requestAnimationFrame(scrollToTop);
+    window.scrollTo(0, c - c / 10);
+  }
+};
+scrollToTopButton.onclick = function(e) {
+  e.preventDefault();
+  scrollToTop();
+}
